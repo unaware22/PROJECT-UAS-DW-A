4530bfd82f640e7ca118a36b3245cbd05ee08ffb
AndraTeguh <andra.teguh.ramadhan@gmail.com>
2024-12-05 12:49:28 +0700

    Edit by Andra Teguh - 4523210017

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Toggle nav
-const navbarBtn = document.getElementById("navbar-btn");
-const mobileMenu = document.getElementById("mobile-menu");
+const navbarBtn = document.getElementById('navbar-btn');
+const mobileMenu = document.getElementById('mobile-menu');
 
 navbarBtn.addEventListener("click", () => {
   mobileMenu.classList.toggle("hidden");
