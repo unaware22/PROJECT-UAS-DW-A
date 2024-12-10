05f2e6a9d528a7c4c2f9c9f6ee9f2fd5e4948793
unaware22 <reaperb910@gmail.com>
2024-12-10 20:45:42 +0700

    Edit by AdityaNurLintang-4523210003

--- script.js
+++ script.js
@@ -370,5 +370,46 @@ const backgroundClass = isFirstDay ? 'bg-white text-black' : 'bg-transparent tex
     }
   });
 
-  
 
+  function showPopup(imageSrc, title, description, rating, price) {
+    // Mengatur konten popup
+    document.getElementById('popup-img').src = imageSrc;
+    document.getElementById('popup-title').textContent = title;
+    document.getElementById('popup-description').textContent = description;
+    document.getElementById('popup-price').textContent = `Rp. ${price.toLocaleString()}`;
+  
+    // Menambahkan rating bintang
+    const ratingContainer = document.getElementById('popup-rating');
+    ratingContainer.innerHTML = ''; // Bersihkan rating sebelumnya
+    const fullStars = Math.floor(rating);
+    const halfStar = rating % 1 !== 0;
+  
+    for (let i = 0; i < fullStars; i++) {
+      ratingContainer.innerHTML += `
+        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24" class="w-5 h-5">
+          <path d="M11.48 3.5a.562.562 0 0 1 1.04 0l2.125 5.11a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.98 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557L3.04 9.985a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z"/>
+        </svg>`;
+    }
+  
+    if (halfStar) {
+      ratingContainer.innerHTML += `
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-5 h-5">
+          <defs>
+            <linearGradient id="halfStar">
+              <stop offset="50%" stop-color="currentColor"></stop>
+              <stop offset="50%" stop-color="transparent"></stop>
+            </linearGradient>
+          </defs>
+          <path fill="url(#halfStar)" d="M11.48 3.5a.562.562 0 0 1 1.04 0l2.125 5.11a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.98 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557L3.04 9.985a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z"/>
+        </svg>`;
+    }
+  
+    // Tampilkan popup
+    document.getElementById('popup').classList.remove('hidden');
+  }
+  
+  function closePopup() {
+    // Sembunyikan popup
+    document.getElementById('popup').classList.add('hidden');
+  }
+  
